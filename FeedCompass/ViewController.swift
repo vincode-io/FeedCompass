645cdb0a1a821a8baedc828b276463df95ef7e79
Maurice Parker <mo@vincode.io>
2019-03-10 09:35:05 -0500

    Initial Commit

---
+++ FeedCompass/ViewController.swift
@@ -0,0 +1,21 @@
+//Copyright © 2019 Vincode, Inc. All rights reserved.
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
