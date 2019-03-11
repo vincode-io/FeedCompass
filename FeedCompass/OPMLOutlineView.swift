e4158eef913bd6588577d48a9bffdf4a97f507f9
Maurice Parker <mo@vincode.io>
2019-03-11 18:05:31 -0500

    Made some styling changes to more closely match NetNewsWire 5.

---
+++ FeedCompass/OPMLOutlineView.swift
@@ -0,0 +1,13 @@
+//Copyright © 2019 Vincode, Inc. All rights reserved.
+
+import Cocoa
+
+class OPMLOutlineView: NSOutlineView {
+
+	override func frameOfCell(atColumn column: Int, row: Int) -> NSRect {
+		var frame = super.frameOfCell(atColumn: column, row: row)
+		frame.origin.x += 4.0
+		frame.size.width -= 4.0
+		return frame
+	}
+}
