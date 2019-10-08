9abb5a571c341efbcc662239ddf040dfe5bf85a1
Maurice Parker <mo@vincode.io>
2019-10-08 14:33:28 -0500

    Add submodule checkout

--- scripts/full-build.sh
+++ scripts/full-build.sh
@@ -5,17 +5,15 @@ set -e
 openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in scripts/certs/dev.cer.enc -d -a -out scripts/certs/dev.cer
 openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in scripts/certs/dev.p12.enc -d -a -out scripts/certs/dev.p12
 
-security create-keychain -p github-actions ios-build.keychain
-security import ./scripts/certs/apple.cer -k ~/Library/Keychains/ios-build.keychain -A
-security import ./scripts/certs/dev.cer -k ~/Library/Keychains/ios-build.keychain -A
-security import ./scripts/certs/dev.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_SECRET -A
+security create-keychain -p github-actions github-build.keychain
+security import ./scripts/certs/apple.cer -k ~/Library/Keychains/github-build.keychain -A
+security import ./scripts/certs/dev.cer -k ~/Library/Keychains/github-build.keychain -A
+security import ./scripts/certs/dev.p12 -k ~/Library/Keychains/github-build.keychain -P $KEY_SECRET -A
+security default-keychain -s github-build.keychain
 
 rm -f ./scripts/certs/dev.cer
 rm -f ./scripts/certs/dev.p12
 
-security default-keychain -s ios-build.keychain
-security set-key-partition-list -S apple-tool:,apple: -s -k github-actions ios-build.keychain
-
 xcodebuild -scheme 'Feed Compass' -configuration Release -allowProvisioningUpdates -showBuildTimingSummary
 
-security delete-keychain ios-build.keychain
+security delete-keychain github-build.keychain
