6a02267fd18b7c67fd203fa22b5c3e84ba15290f
Maurice Parker <mo@vincode.io>
2019-10-08 14:21:00 -0500

    Revert to using separate job steps

--- scripts/create-keychain.sh
+++ scripts/create-keychain.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 set -v
+
 openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in scripts/certs/dist.cer.enc -d -a -out scripts/certs/dist.cer
 openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in scripts/certs/dist.p12.enc -d -a -out scripts/certs/dist.p12
-openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in scripts/certs/mac-dev.p12.enc -d -a -out scripts/certs/mac-dev.p12
-security create-keychain -p github-actions ios-build.keychain
-security import ./scripts/certs/apple.cer -k ~/Library/Keychains/ios-build.keychain -A
-security import ./scripts/certs/dist.cer -k ~/Library/Keychains/ios-build.keychain -A
-security import ./scripts/certs/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_SECRET -A
-security import ./scripts/certs/mac-dev.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_SECRET -A
+
+security create-keychain -p github-actions github-build.keychain
+security import ./scripts/certs/apple.cer -k ~/Library/Keychains/github-build.keychain -A
+security import ./scripts/certs/dist.cer -k ~/Library/Keychains/github-build.keychain -A
+security import ./scripts/certs/dist.p12 -k ~/Library/Keychains/github-build.keychain -P $KEY_SECRET -A
+
 rm -f ./scripts/certs/dist.cer
 rm -f ./scripts/certs/dist.p12
-rm -f ./scripts/certs/mac-dev.p12
-security set-key-partition-list -S apple-tool:,apple: -s -k github-actions ios-build.keychain
+
+security default-keychain -s github-build.keychain
+#security set-key-partition-list -S apple-tool:,apple: -s -k github-actions github-build.keychain
