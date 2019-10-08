7d497cdfe6603834ab403834293187d39178b94c
Maurice Parker <mo@vincode.io>
2019-10-08 15:41:41 -0500

    Set ACL on keychain

--- scripts/full-build.sh
+++ scripts/full-build.sh
@@ -9,10 +9,11 @@ security create-keychain -p github-actions github-build.keychain
 security import ./scripts/certs/apple.cer -k ~/Library/Keychains/github-build.keychain -A
 security import ./scripts/certs/dev.cer -k ~/Library/Keychains/github-build.keychain -A
 security import ./scripts/certs/dev.p12 -k ~/Library/Keychains/github-build.keychain -P $KEY_SECRET -A
+security set-key-partition-list -S apple-tool:,apple: -s -k github-actions github-build.keychain
 security default-keychain -s github-build.keychain
 
-security set-keychain-settings -t 3600 -l github-build.keychain
-security unlock-keychain -p github-actions github-build.keychain
+#security set-keychain-settings -t 3600 -l github-build.keychain
+#security unlock-keychain -p github-actions github-build.keychain
 
 rm -f ./scripts/certs/dev.cer
 rm -f ./scripts/certs/dev.p12
