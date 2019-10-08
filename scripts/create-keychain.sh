aeb446953dd538bbbc3ce106b78b808d8d7d4d5e
Maurice Parker <mo@vincode.io>
2019-10-08 11:08:56 -0500

    Corrected keychain script to point at correct files

--- scripts/create-keychain.sh
+++ scripts/create-keychain.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -v
-openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in scripts/certs/dist.cer.enc -d -a -out scripts/certs/dist.cer
-openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in scripts/certs/dist.p12.enc -d -a -out scripts/certs/dist.p12
+openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in scripts/certs/dev.cer.enc -d -a -out scripts/certs/dev.cer
+openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in scripts/certs/dev.p12.enc -d -a -out scripts/certs/dev.p12
 security create-keychain -p github-actions ios-build.keychain
 security import ./scripts/certs/apple.cer -k ~/Library/Keychains/ios-build.keychain -A
-security import ./scripts/certs/dist.cer -k ~/Library/Keychains/ios-build.keychain -A
-security import ./scripts/certs/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_SECRET -A
-rm -f ./scripts/certs/dist.cer
-rm -f ./scripts/certs/dist.p12
+security import ./scripts/certs/dev.cer -k ~/Library/Keychains/ios-build.keychain -A
+security import ./scripts/certs/dev.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_SECRET -A
+rm -f ./scripts/certs/dev.cer
+rm -f ./scripts/certs/dev.p12
 security set-key-partition-list -S apple-tool:,apple: -s -k github-actions ios-build.keychain
