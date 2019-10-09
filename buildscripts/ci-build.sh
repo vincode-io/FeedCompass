dc3080a0bd9e5bbef74c345338f586e8315548f9
Maurice Parker <mo@vincode.io>
2019-10-09 15:44:36 -0500

    Change build to point to new dev cert/key

--- buildscripts/ci-build.sh
+++ buildscripts/ci-build.sh
@@ -4,14 +4,14 @@ set -e
 
 # Unencrypt our provisioning profile, certificate, and private key
 openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in buildscripts/profile/FeedCompass.provisionprofile.enc -d -a -out buildscripts/profile/FeedCompass.provisionprofile
-openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in buildscripts/certs/dist.cer.enc -d -a -out buildscripts/certs/dist.cer
-openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in buildscripts/certs/dist.p12.enc -d -a -out buildscripts/certs/dist.p12
+openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in buildscripts/certs/dev.cer.enc -d -a -out buildscripts/certs/dev.cer
+openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in buildscripts/certs/dev.p12.enc -d -a -out buildscripts/certs/dev.p12
 
 # Put the certificates and private key in the Keychain, set ACL permissions, and make default
 security create-keychain -p github-actions github-build.keychain
 security import buildscripts/certs/apple.cer -k ~/Library/Keychains/github-build.keychain -A
-security import buildscripts/certs/dist.cer -k ~/Library/Keychains/github-build.keychain -A
-security import buildscripts/certs/dist.p12 -k ~/Library/Keychains/github-build.keychain -P $KEY_SECRET -A
+security import buildscripts/certs/dev.cer -k ~/Library/Keychains/github-build.keychain -A
+security import buildscripts/certs/dev.p12 -k ~/Library/Keychains/github-build.keychain -P $KEY_SECRET -A
 security set-key-partition-list -S apple-tool:,apple: -s -k github-actions github-build.keychain
 security default-keychain -s github-build.keychain
 
@@ -21,8 +21,8 @@ cp buildscripts/profile/FeedCompass.provisionprofile ~/Library/MobileDevice/Prov
 
 # Delete the decrypted files
 rm -f buildscripts/profile/FeedCompass.provisionprofile
-rm -f buildscripts/certs/dist.cer
-rm -f buildscripts/certs/dist.p12
+rm -f buildscripts/certs/dev.cer
+rm -f buildscripts/certs/dev.p12
 
 # Do the build
 xcodebuild -scheme 'Feed Compass' -configuration Release -allowProvisioningUpdates -showBuildTimingSummary
